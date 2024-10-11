03fb9a5bb2971aa5981faef85c38a8a5a3d36bef
Matthew Dolan <mdolancode@gmail.com>
2024-10-11 15:58:08 -0700

    First commit of the project to the repo.

---
+++ fail-hard/fail-hard/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  fail-hard
+//
+//  Created by Matthew Dolan on 2024-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fail-hard/fail-hard/fail_hardApp.swift
@@ -0,0 +1,17 @@
+//
+//  fail_hardApp.swift
+//  fail-hard
+//
+//  Created by Matthew Dolan on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct fail_hardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
