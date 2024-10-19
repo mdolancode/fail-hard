3f4693e2ea52feb80e33ab27d27d9baabb2d0b0b
Matthew Dolan <mdolancode@gmail.com>
2024-10-19 08:37:15 -0700

    Created workout list view to edit workouts.

---
+++ fail-hard/fail-hard/WorkoutListView.swift
@@ -0,0 +1,55 @@
+//
+//  WorkoutListView.swift
+//  fail-hard
+//
+//  Created by Matthew Dolan on 2024-10-16.
+//
+
+import SwiftUI
+import CoreData
+
+struct WorkoutListView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(entity: Workout.entity(),
+                  sortDescriptors:[NSSortDescriptor(keyPath: \Workout.date, ascending: true)],
+                  animation: .default)
+    private var workouts: FetchedResults<Workout>
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(workouts) { workout in
+                    NavigationLink(destination: EditWorkoutView(workout: workout)) {
+                        Text(workout.name ?? "Unnamed Workout")
+                    }
+                }
+                .onDelete(perform: deleteWorkouts)
+            }
+            .navigationTitle("Workouts")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+            }
+        }
+    }
+    
+    private func deleteWorkouts(offsets: IndexSet) {
+        withAnimation {
+            offsets.map {workouts[$0] }.forEach(viewContext.delete)
+            do {
+                try viewContext.save()
+            } catch {
+                // Handle the error appropriately
+                print("Failed to delete workout: \(error.localizedDescription)")
+            }
+        }
+        
+    }
+}
+
+struct WorkoutListView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutListView()
+    }
+}
